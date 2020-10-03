be9a524530ceb399b1b6b1a3e417827b93b60e36
Iván Gallego Torres <id.gallego16@iesdoctorbalmis.com>
2020-10-03 16:25:59 +0200

    03/10/2020

--- DWEC/T1/exercise-week2/exercise2.js
+++ DWEC/T1/exercise-week2/exercise2.js
@@ -27,10 +27,9 @@ window.addEventListener("DOMContentLoaded", e => {
         } else {
             let card = document.createElement("div");
             card.classList.add("card", "shadow");
-            let image = document.createElement("image");
+            let image = document.createElement("img");
             image.classList.add("card-img-top");
             image.setAttribute("src", document.getElementById("imgPreview").getAttribute("src"));
-            //image.setAttribute("src", newProductForm.image.value);
             let cardBody = document.createElement("div");
             cardBody.classList.add("card-body");
             let cardTitle = document.createElement("h5");
@@ -48,31 +47,14 @@ window.addEventListener("DOMContentLoaded", e => {
             colRight.classList.add("col", "text-right");
             colRight.innerText = newProductForm.price.value + " €";
 
-            card.appendChild(image);
-            card.appendChild(cardBody);
-            card.appendChild(cardFooter);
-            cardBody.appendChild(cardTitle);
-            cardBody.appendChild(cardText);
-            cardFooter.appendChild(col);
-            cardFooter.appendChild(colRight);
+            card.append(image, cardBody, cardFooter);
+            cardBody.append(cardTitle, cardText);
+            cardFooter.append(col, colRight);
 
             document.getElementById("productsContainer").appendChild(card);
 
             newProductForm.reset();
-
-            /*
-            <div class="card shadow">
-                <img class="card-img-top" src="./img/chicken.jpg">
-                <div class="card-body">
-                    <h5 class="card-title">Product title</h5>
-                    <p class="card-text">This is the description.</p>
-                </div>
-                <div class="card-footer bg-transparent text-muted row">
-                    <div class="col">Category</div>
-                    <div class="col text-right">Price €</div>
-                </div>
-            </div>
-            */
+            document.getElementById("imgPreview").src = "";
         }
     });
 });
