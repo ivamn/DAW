1dafab2ad4b9f1a37bb4d384b1f31b1bedc797a4
Iván Daniel Gallego Torres <id.gallego16@iesdoctorbalmis.com>
2020-10-26 14:47:59 +0100

    26/10/20 M

--- DAW/Pruebas/PruebasMongo/index.js
+++ DAW/Pruebas/PruebasMongo/index.js
@@ -3,6 +3,29 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017/contactos',
     { useNewUrlParser: true });
 
+let restauranteSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    direccion: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    telefono: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: /^\d{9}$/
+    }
+});
+let Restaurante = mongoose.model('restaurante', restauranteSchema);
+
 let contactoSchema = new mongoose.Schema({
     nombre: {
         type: String,
@@ -21,31 +44,11 @@ let contactoSchema = new mongoose.Schema({
         type: Number,
         min: 18,
         max: 120
+    },
+    restauranteFavorito: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'restaurante'
     }
 });
 
 let Contacto = mongoose.model('contactos', contactoSchema);
-
-let contacto1 = new Contacto({
-    nombre: "Nacho",
-    telefono: "966112233",
-    edad: 41
-});
-
-let contacto2 = new Contacto({
-    nombre: "Pepecebo",
-    telefono: "912456789",
-    edad: 184984
-});
-
-contacto1.save().then(resultado => {
-    console.log("Contacto añadido:", resultado);
-}).catch(error => {
-    console.log("ERROR añadiendo contacto:", error);
-});
-
-contacto2.save().then(resultado => {
-    console.log("Contacto añadido:", resultado);
-}).catch(error => {
-    console.log("ERROR añadiendo contacto:", error);
-});
