c201d9db28d837fcfa2f3a7c92699eed80643089
Iván Gallego Torres <id.gallego16@iesdoctorbalmis.com>
2020-11-10 18:40:01 +0100

    10/11/2020 DWEC

--- DWEC/T2/ivan-gallego-exercise-week7/src/app/app.module.ts
+++ DWEC/T2/ivan-gallego-exercise-week7/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductsPageComponent } from './products-page/products-page.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ProductsPageComponent } from './products-page/products-page.component';
     ProductsPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- DWEC/T2/ivan-gallego-exercise-week7/src/app/products-page/products-page.component.ts
+++ DWEC/T2/ivan-gallego-exercise-week7/src/app/products-page/products-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'sp-products-page',
@@ -7,12 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsPageComponent implements OnInit {
 
-  products = [];
-  newPorduct = null;
+  products: Product[] = [];
+  newProduct!: Product;
+  imageFile = "";
+  categories = ["None", "Electronics", "Motor and vehicles", "Sports and hobbies", "Other"];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.resetProduct();
+  }
+
+  submitForm(): void {
+    this.products.push(this.newProduct);
+    this.resetProduct();
+  }
+
+  changeImage(fileInput: HTMLInputElement): void {
+    if (!fileInput.files || fileInput.files.length === 0) { return; }
+    const reader: FileReader = new FileReader();
+    reader.readAsDataURL(fileInput.files[0]);
+    reader.addEventListener('loadend', e => {
+      this.newProduct.mainPhoto = reader.result as string;
+    });
+  }
+
+  resetProduct(): void {
+    this.newProduct = {
+      category: 0,
+      description: "",
+      mainPhoto: "",
+      price: 0,
+      title: ""
+    }
+    this.imageFile = "";
   }
 
 }
