cd32e40f1f17fcce829eaa9ddb9bbad539a09e3f
Iván Daniel Gallego Torres <id.gallego16@iesdoctorbalmis.com>
2021-01-25 14:30:03 +0100

    ProdAsturianos terminados

--- DAW/Ejercicios/ProdAsturianos_V3/index.js
+++ DAW/Ejercicios/ProdAsturianos_V3/index.js
@@ -30,13 +30,7 @@ app.set('view engine', 'njk');
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        let method = req.body._method;
-        delete req.body._method;
-        return method;
-    }
-}));
+app.use(methodOverride('_method'));
 
 app.use(session({
     secret: '1234',
